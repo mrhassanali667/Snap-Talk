4d429d5a972579db009364a90df8a2fbe0cd4e9d
Hassan-Ali0 <hassanali031935@gmail.com>
2025-11-16 15:31:11 +0500

    fixes

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,11 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import 'dotenv/config'
 
+const apiKey = import.meta.env.VITE_FIREBASE_KEY
 
-
-const firebaseConfig = process.env.FIREBASE_CONFIG;
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: "snap-talk-f961b.firebaseapp.com",
+    projectId: "snap-talk-f961b",
+    storageBucket: "snap-talk-f961b.firebasestorage.app",
+    messagingSenderId: "1011091844571",
+    appId: "1:1011091844571:web:db9fb4d093faae038d0bfe"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
