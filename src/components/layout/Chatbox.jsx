4c0e61eaea1befe03bb4edc70914a63c88b88cc1
Hassan-Ali0 <hassanali031935@gmail.com>
2025-12-07 13:43:40 +0500

    sizing

--- src/components/layout/Chatbox.jsx
+++ src/components/layout/Chatbox.jsx
@@ -8,7 +8,7 @@ const Chatbox = () => {
   let isShowChat = useSelector(state => state.chat.isShowChat)
 
   return (
-    <div className='h-ful w-full flex gap-3 max-lg:h-screen max-lg:w-screen bg-white dark:bg-gray-900 text-white p-5 max-lg:absolute'>
+    <div className='h-ful w-full flex gap-3 max-lg:h-[100dvh] max-lg:w-screen bg-white dark:bg-gray-900 text-white p-5 max-lg:absolute'>
       {innerWidth < 1024 &&
         <button onClick={() => dispatch(hideChat())} className='h-10 border-1 p-2'>
           back
--- src/components/specific/UsersList.jsx
+++ src/components/specific/UsersList.jsx
@@ -16,7 +16,7 @@ const UsersList = () => {
 
   return (
     <div onScroll={handelScrollBar} className={`h-full w-full flex gap-1 flex-col overflow-y-auto hide-scroll ${showScrollBar ? 'users-list' : ''}`}>
-      {[...Array(20)].map((_, index) =>
+      {[...Array(2)].map((_, index) =>
         <div onClick={() => dispatch(showChat())} key={index} className='min-h-[70px] w-full bg-slate-200 dark:bg-gray-900  flex flex-col justify-end items-center '>
 
         </div>
