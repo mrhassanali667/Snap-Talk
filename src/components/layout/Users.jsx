171a6da8e7d4ed356ce55692c95c37bd3a6ef285
Hassan-Ali0 <hassanali031935@gmail.com>
2025-12-08 19:23:05 +0500

    fixes

--- src/components/layout/Users.jsx
+++ src/components/layout/Users.jsx
@@ -5,14 +5,14 @@ import UsersList from '../specific/UsersList.jsx'
 
 const Users = () => {
   return (
-    <div className='h-full w-full flex flex-col gap-1 bg-slate-100 dark:bg-gray-800/70 py-5 overflow-hidden'>
-      <div className='flex flex-col gap-3'>
+    <div className='h-full w-full flex flex-col  bg-slate-100 dark:bg-gray-800/70 py-5 overflow-hidden'>
+      <div className='flex flex-col gap-1 lg:gap-3'>
         <Heading name={'Chats'} />
         <div className='py-2 px-6 lg:px-6 '>
           <SearchBar />
         </div>
       </div>
-      <div className='min-h-[75px] w-full py-2 px-4 flex  gap-4 justify-around relative overflow-x-auto x-scroll '>
+      <div className='min-h-[75px] w-full py-2 px-6 flex  gap-4 relative overflow-x-auto x-scroll '>
         {[...Array(4)].map((_, index) =>
           <div key={index} className='flex flex-col justify-center items-center '>
               <div className='h-[60px] w-[60px] rounded-full bg-slate-300 dark:bg-gray-900 border-2 border-indigo-500 '></div>
@@ -20,7 +20,7 @@ const Users = () => {
         )}
       </div>
       <div>
-        <h2 className='px-6 pt-[3px] pb-3 text-[1em] font-semibold font-manrope  dark:text-slate-100 text-zinc-700'>Recent</h2>
+        <h2 className='px-6 pt-[4px] pb-2 text-[1em] font-semibold font-manrope  dark:text-slate-100 text-zinc-700'>Recent</h2>
       </div>
       <div className='h-[80%] w-full'>
       <UsersList/>
