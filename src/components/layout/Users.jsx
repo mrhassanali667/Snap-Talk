cc13985804b8996acfcd0cd0a6289c89888a90ce
Hassan-Ali0 <hassanali031935@gmail.com>
2025-12-08 20:16:09 +0500

    add some styling

--- src/components/layout/Users.jsx
+++ src/components/layout/Users.jsx
@@ -12,18 +12,25 @@ const Users = () => {
           <SearchBar />
         </div>
       </div>
-      <div className='min-h-[75px] w-full py-2 px-6 flex  gap-4 relative overflow-x-auto x-scroll '>
-        {[...Array(4)].map((_, index) =>
-          <div key={index} className='flex flex-col justify-center items-center '>
-              <div className='h-[60px] w-[60px] rounded-full bg-slate-300 dark:bg-gray-900 border-2 border-indigo-500 '></div>
-          </div>
-        )}
+      <div className='pr-2'>
+        <div className='min-h-[120px] h-[100px]  w-full py-2 px-5 flex  gap-1 relative overflow-x-auto x-scroll '>
+          {["Hassan", "Usama", "Sufiyan", "Huzaifa", "Ali", "Murtaza", "Zohan"].map((name, index) =>
+            <div key={index} className='h-full min-w-[70px] flex flex-col  items-center  cursor-pointer  '>
+              <div>
+                <div className='h-[55px] w-[55px] rounded-full bg-slate-300 dark:bg-gray-900 border-2 border-indigo-500 '></div>
+              </div>
+              <div>
+                <p className='text-[0.85em] dark:text-slate-200 text-center text-zinc-700 font-semibold mt-2'>{name}</p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
       <div>
         <h2 className='px-6 pt-[4px] pb-2 text-[1em] font-semibold font-manrope  dark:text-slate-100 text-zinc-700'>Recent</h2>
       </div>
       <div className='h-[80%] w-full'>
-      <UsersList/>
+        <UsersList />
       </div>
     </div>
   )
