a1c9391cddfb991528a3f2ff0d385a0a957cf320
Hassan-Ali0 <hassanali031935@gmail.com>
2025-12-18 23:55:06 +0500

    update UI

--- src/components/layout/Chatbox.jsx
+++ src/components/layout/Chatbox.jsx
@@ -11,15 +11,15 @@ const Chatbox = () => {
 
   return (
     <div className='h-full w-full flex flex-col max-lg:h-[100dvh] max-lg:w-screen bg-white dark:bg-gray-900 text-white max-lg:absolute'>
-      <nav className='min-h-[65px] flex justify-between bg-slate-400'>
+      <nav className='min-h-[75px] flex justify-between bg-white dark:bg-gray-900 border-b-1 border-gray-700'>
         <div className='h-full px-2 flex items-center'>
-          <div className={`h-11 w-11 flex justify-center items-center bg-gray-600 rounded-[50%] border-1`}>
-            {false ?
-              <img src="" alt=""
-                className='h-8 w-8 rounded-[50%]'
+          <div className={`h-10 w-10 flex justify-center items-center bg-gray-600 rounded-[50%] border-1`}>
+            {true ?
+              <img src="/images/DP.jpg" alt=""
+                className='h-10 w-10 rounded-[50%]'
               />
               :
-              <h2 className={`text-white text-[1.3em] font-semibold`}>{user?.email[0].toUpperCase()}</h2>
+              <h2 className={`text-white text-[1.3em] font-semibold`}>{user?.username[0].toUpperCase()}</h2>
             }
           </div>
           <h2 className='ml-2 text-white font-semibold'>{user?.username}</h2>
@@ -28,10 +28,10 @@ const Chatbox = () => {
 
         </div>
       </nav>
-      <main className='flex-grow bg-slate-500 dark:bg-gray-800/70 overflow-y-auto'>
+      <main className='flex-grow bg-white dark:bg-gray-900 overflow-y-auto'>
 
       </main>
-      <footer className='min-h-[55px] flex justify-end items-center w-full bg-slate-700 max-lg:dark:bg-gray-900/95 max-lg:px-5 max-lg:py-3 '>
+      <footer className='min-h-[55px] flex justify-end items-center w-full bg-white dark:bg-gray-900 border-t-1  dark:border-gray-700 px-4 '>
 
       </footer>
     </div>
