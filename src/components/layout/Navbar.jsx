1626f41798fefa48882e971e4b4c6a50d16c3118
Hassan-Ali0 <hassanali031935@gmail.com>
2025-12-22 02:05:56 +0500

    refactor: improve Navbar layout and styling for better responsiveness and user experience

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -29,138 +29,140 @@ const Navbar = () => {
   }
 
   return (
-    <div className='max-lg:h-[65px] lg:w-[75px] bg-white dark:bg-gray-800/90 flex lg:flex-col justify-between  items-center py-1 lg:py-4 px-4 max-lg:px-3    '>
-      <NavLink onClick={logout} to={'/'} className='max-lg:hidden' >
-        <img src="/images/logo.png" alt="logo"
-          className='w-[40px]'
-        />
-      </NavLink>
-      <nav className='max-lg:w-[85%] flex lg:flex-col gap-4 max-lg:gap-1 justify-center max-lg:justify-between items-center'>
-        <NavLink to={'/profile'} className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/40' : ''}`}>
-          {
-            ({ isActive }) => (
-
-              <svg
-                className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'}`}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-              </svg>
-            )
-          }
-
+    <div className='max-lg:h-[65px] lg:w-[75px] bg-white dark:bg-gray-800/90 flex lg:flex-col justify-between  items-center py-1 lg:py-4 px-4 max-lg:px-2     '>
+      <div className='max-lg:w-[85%] flex lg:flex-col gap-4 max-lg:justify-between  justify-start items-center'>
+        <NavLink onClick={logout} to={'/'} className='max-lg:hidden' >
+          <img src="/images/logo.png" alt="logo"
+            className='w-[40px]'
+          />
         </NavLink>
-        <NavLink to={'/'} end className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/50' : ''}`}>
-          {
-            ({ isActive }) => (
-              <svg
-                className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'} `}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M16 10.5h.01m-4.01 0h.01M8 10.5h.01M5 5h14a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-6.6a1 1 0 0 0-.69.275l-2.866 2.723A.5.5 0 0 1 8 18.635V17a1 1 0 0 0-1-1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"
-                />
-              </svg>
-            )
-          }
-        </NavLink>
-        <NavLink to={'/groups'} className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/50' : ''}`}>
-          {
-            ({ isActive }) => (
+        <nav className='h-auto w-full flex lg:flex-col gap-2 max-lg:gap-1 justify-center max-lg:justify-between items-center'>
+          <NavLink to={'/profile'} className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/20' : ''}`}>
+            {
+              ({ isActive }) => (
 
-              <svg
-                className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'} `}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeWidth={2}
-                  d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
-                />
-              </svg>
-            )
-          }
-        </NavLink>
-        <NavLink to={'/contacts'} className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/50' : ''}`}>
-          {
-            ({ isActive }) => (
+                <svg
+                  className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'}`}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              )
+            }
 
-              <svg
-                className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'}`}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeWidth={2}
-                  d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-              </svg>
-            )
-          }
-        </NavLink>
-        <NavLink to={'/settings'} className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/50' : ''}`}>
-          {
-            ({ isActive }) => (
+          </NavLink>
+          <NavLink to={'/'} end className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/20' : ''}`}>
+            {
+              ({ isActive }) => (
+                <svg
+                  className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'} `}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M16 10.5h.01m-4.01 0h.01M8 10.5h.01M5 5h14a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-6.6a1 1 0 0 0-.69.275l-2.866 2.723A.5.5 0 0 1 8 18.635V17a1 1 0 0 0-1-1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"
+                  />
+                </svg>
+              )
+            }
+          </NavLink>
+          <NavLink to={'/groups'} className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/20' : ''}`}>
+            {
+              ({ isActive }) => (
 
-              <svg
-                className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'}`}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"
-                />
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-                />
-              </svg>
-            )
-          }
+                <svg
+                  className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'} `}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeWidth={2}
+                    d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
+                  />
+                </svg>
+              )
+            }
+          </NavLink>
+          <NavLink to={'/contacts'} className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/20' : ''}`}>
+            {
+              ({ isActive }) => (
 
-        </NavLink>
-      </nav>
+                <svg
+                  className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'}`}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeWidth={2}
+                    d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              )
+            }
+          </NavLink>
+          <NavLink to={'/settings'} className={({ isActive }) => `navlink ${isActive ? 'bg-violet-50/70 dark:bg-gray-600/20' : ''}`}>
+            {
+              ({ isActive }) => (
+
+                <svg
+                  className={`h-7 w-7 lg:w-8 lg:h-8  ${isActive ? 'text-[#7269ef] ' : 'text-zinc-400'}`}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"
+                  />
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                  />
+                </svg>
+              )
+            }
+
+          </NavLink>
+        </nav>
+      </div>
       <div className='flex lg:flex-col gap-8 justify-center items-center '>
         <button
           onClick={toggleTheme}
